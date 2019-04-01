e27df0918ce077e19c02fc7b23fbe16d598707a9
Tomasz Felczak <janusz.snieg89@gmail.com>
2019-04-01 18:33:37 +0200

    Updates

---
+++ precache-manifest.68afa43455d67d43fc74dc480ebaf6a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "792e7991af8694315ea1",
+    "url": "/poke_search/static/js/runtime~main.1c36989a.js"
+  },
+  {
+    "revision": "8805e2e22119d0a178a0",
+    "url": "/poke_search/static/js/main.c0b53d23.chunk.js"
+  },
+  {
+    "revision": "66902b44f545669983de",
+    "url": "/poke_search/static/js/2.47bf6d28.chunk.js"
+  },
+  {
+    "revision": "8805e2e22119d0a178a0",
+    "url": "/poke_search/static/css/main.5e245e4a.chunk.css"
+  },
+  {
+    "revision": "58af8b714a46f6d10e3bc5b9570f7a70",
+    "url": "/poke_search/index.html"
+  }
+];
