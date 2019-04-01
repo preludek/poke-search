9d0a0227f630d00cb4eb807e9de3c08a175e7a63
Tomasz Felczak <janusz.snieg89@gmail.com>
2019-04-01 10:41:27 +0200

    Added search results display

--- src/History/History.js
+++ src/History/History.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Table, Header, Segment } from 'semantic-ui-react';
+import { Table, Header, Segment, Image } from 'semantic-ui-react';
 
 class History extends Component {
     state = {
@@ -10,6 +10,35 @@ class History extends Component {
     };
 
     render() {
+        let result = null
+        if (this.props.pkmSearches.id) {
+            result = (
+                <Segment
+                    textAlign="center"
+                    style={{ width: '600px', margin: '20px auto', minHeight: '300px' }}
+                >
+                    <Header style={{ margin: '20px auto', fontSize: '1.6rem' }}>{this.props.pkmSearches.name.toUpperCase()}</Header>
+                    <Table basic='very' celled collapsing style={{ width: '41%', display: 'inline-block' }} >
+                        <Table.Row>
+                            <Table.Cell style={{ fontWeight: 'bold' }}>Id</Table.Cell>
+                            <Table.Cell>{this.props.pkmSearches.id}</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell style={{ fontWeight: 'bold' }}>Type</Table.Cell>
+                            <Table.Cell>{
+                                this.props.pkmSearches.types.map((type => <Table.Cell key={type} style={{ borderTop: 'none' }}>{type.type.name}</Table.Cell>))
+                            }</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell style={{ fontWeight: 'bold' }}>Height/ Weight</Table.Cell>
+                            <Table.Cell>{this.props.pkmSearches.height * 10} cm / {this.props.pkmSearches.weight / 10} kg</Table.Cell>
+                        </Table.Row>
+                    </Table>
+                    <Image src={this.props.pkmSearches.sprites.front_default} size='small' floated='right' style={{ marginRight: '50px' }} />
+                </Segment>
+
+            )
+        }
         const history = this.props.historySearches.map((search => (
             <Table.Row key={search.id}>
                 <Table.Cell>{search.id}</Table.Cell>
@@ -28,9 +57,10 @@ class History extends Component {
                     style={{ minHeight: '50vh', width: '900px', margin: '100px auto 0 auto' }}
                     vertical
                 >
+                    {result}
                     <Header>Search History:</Header>
                     <Table celled>
-                        <Table.Header>
+                        <Table.Header style={{ textAlign: 'center' }}>
                             <Table.Row>
                                 <Table.HeaderCell>Id</Table.HeaderCell>
                                 <Table.HeaderCell>Name</Table.HeaderCell>
